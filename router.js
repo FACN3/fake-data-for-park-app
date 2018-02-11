d109ba97b9638adfe3353a22d0a04fb54f0195d3
Idan Tabachnik <idantb101@gmail.com>
2018-02-11 11:11:19 +0200

    init app

---
+++ router.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const routes = () => {
+  let router = express.Router();
+
+  router.route('/allParks').get((req, res) => {
+    res.json([
+      {
+        parkId: 1,
+        parkName: 'shahne',
+        parkDesc: 'a big park',
+        parkCoordinates: { lat: 100, lang: 100 },
+        raiting: 3,
+        wazeLink: 'fake Waze link',
+        tags: ['tag1', 'tag2', 'tag3'],
+        picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+      },
+      {
+        parkId: 1,
+        parkName: 'shahne',
+        parkDesc: 'a big park',
+        parkCoordinates: { lat: 100, lang: 100 },
+        raiting: 3,
+        wazeLink: 'fake Waze link',
+        tags: ['tag1', 'tag2', 'tag3'],
+        picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+      },
+      {
+        parkId: 1,
+        parkName: 'shahne',
+        parkDesc: 'a big park',
+        parkCoordinates: { lat: 100, lang: 100 },
+        raiting: 3,
+        wazeLink: 'fake Waze link',
+        tags: ['tag1', 'tag2', 'tag3'],
+        picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+      },
+      {
+        parkId: 1,
+        parkName: 'shahne',
+        parkDesc: 'a big park',
+        parkCoordinates: { lat: 100, lang: 100 },
+        raiting: 3,
+        wazeLink: 'fake Waze link',
+        tags: ['tag1', 'tag2', 'tag3'],
+        picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+      },
+      {
+        parkId: 1,
+        parkName: 'shahne',
+        parkDesc: 'a big park',
+        parkCoordinates: { lat: 100, lang: 100 },
+        raiting: 3,
+        wazeLink: 'fake Waze link',
+        tags: ['tag1', 'tag2', 'tag3'],
+        picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+      }
+    ]);
+  });
+
+  router.route('/singlePark').get((req, res) => {
+    res.json({
+      parkId: 1,
+      parkName: 'shahne',
+      parkDesc: 'a big park',
+      parkCoordinates: { lat: 100, lang: 100 },
+      raiting: 3,
+      wazeLink: 'fake Waze link',
+      tags: ['tag1', 'tag2', 'tag3'],
+      picturesUrl: { small: ['fakeUrl', 'fakeUrl'], big: 'fakeUrl' }
+    });
+  });
+
+  return router;
+};
+
+module.exports = routes;
